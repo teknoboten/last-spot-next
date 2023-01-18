8ee4867ef88a3d6ec0d38600786cb4a4d61f687c
serra <serra@boten.ca>
2023-01-18 11:14:41 -0800

    initial!

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,65 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify";
+
+
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      authorization:
+        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({token, account}) {
+      if (account) {
+        token.accessToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session(session, user) {
+      session.user = user;
+      return session;
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     SpotifyProvider({
+//       clientId: process.env.SPOTIFY_CLIENT_ID,
+//       clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+//     }),
+//     // ...add more providers here
+//   ],
+    
+//   callbacks: {
+//     async redirect({ url, baseUrl }) {
+//       // Allows relative callback URLs
+//       if (url.startsWith("/")) return `${baseUrl}${url}`
+//       // Allows callback URLs on the same origin
+//       else if (new URL(url).origin === baseUrl) return url
+//       return baseUrl
+//     }
+//   }
+// }
+
+// export default NextAuth(authOptions)
